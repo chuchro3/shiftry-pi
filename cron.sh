2f8c4a7bf130abdc0d3895a46d3e47a8a0334f54
Robert Chuchro <robertchuchro@gmail.com>
2020-05-19 11:01:33 -0700

    adding scp command to send over log file

--- cron.sh
+++ cron.sh
@@ -4,4 +4,6 @@ git -C /home/pi/Repos/shiftry-pi/ pull
 
 python3 /home/pi/Repos/shiftry-pi/send_data.py /home/pi/.ssh/shiftry.pem ec2-user@52.10.49.156 /home/ec2-user/data
 
+scp -i /home/pi/.ssh/shiftry.pem /home/pi/logs/shiftry.log ec2-user@52.10.49.156:/home/ec2-user/data
+
 sudo halt
